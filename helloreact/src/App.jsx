032b43facb4ff055722de86453148a479ea7a306
luanjollembeck-stack <Luan-jollembeck@estudante.sesisenai.org.br>
2026-02-11 22:18:03 -0300

    adiciono primeiro projeto do senai

---
+++ helloreact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Cabecalho from './components/Cabecalho';
+import Corpo from './components/Corpo';
+
+function App() {
+  
+
+  return (
+    <>
+    <Cabecalho/>
+    <Corpo/>
+    </>
+  )
+}
+
+export default App
---
+++ helloreact/src/components/Corpo.jsx
@@ -0,0 +1,13 @@
+
+
+function Corpo() {
+  return (
+    <div>
+      <h1>Corpo da página!</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia expedita asperiores soluta tempora labore fugiat aliquid, ea quasi repellendus, optio rerum possimus impedit perspiciatis laborum excepturi dicta, delectus temporibus atque.</p>
+      
+    </div>
+  )
+}
+
+export default Corpo
