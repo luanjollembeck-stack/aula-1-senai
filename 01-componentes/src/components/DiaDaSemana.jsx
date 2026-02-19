10268fdcb05f176100cbcec49b5d444042a83941
luanjollembeck-stack <Luan-jollembeck@estudante.sesisenai.org.br>
2026-02-18 22:23:17 -0300

    criamos o projeto básico

---
+++ 01-componentes/src/components/DiaDaSemana.jsx
@@ -0,0 +1,50 @@
+
+function DiaDaSemana(){
+    function verificarDia(){
+        let dia = prompt("Digite o dia da Semana (1 a 7): ")
+        dia = Number(dia)
+
+            // if (dia == 1) {
+            //     alert("Domingo")
+            // }else if(dia == 2){
+            //     alert("Segunda")
+            // }else if(dia == 3){
+            //     alert("Terça")
+            // }else if(dia == 4){
+            //     alert("Quarta")
+            // }else if(dia == 5){
+            //     alert("Quinta")
+            // }else if(dia == 6){
+            //     alert("Sexta")
+            // }else if(dia == 7){
+            //     alert("Sábado")
+            // }else{
+            //     alert("Esse dia não existe")
+            switch(dia){
+                case 1: alert("Domingo")
+                break
+                case 2: alert("Segunda")
+                break
+                case 3: alert("Terça")
+                break
+                case 4: alert("Quarta")
+                break
+                case 5: alert("Quinta")
+                break
+                case 6: alert("Sexta")
+                break
+                case 7: alert("Sábado")
+                break
+                default: alert("Esse dia não existe")
+            }
+        }
+        return(
+            <div>
+            <h2>Dia da semana</h2>
+            <button onClick={verificarDia}>Verificar dia</button>
+        </div>
+    )
+}
+
+
+export default DiaDaSemana
