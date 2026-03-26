28f827eb583514f6c8ea80afc6c8e8feac2737d3
luanjollembeck-stack <Luan-jollembeck@estudante.sesisenai.org.br>
2026-03-25 22:18:31 -0300

    nova matéria DENOVO

---
+++ 04-layout/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="cont-app">
+   <header className={'cont-header'}>nome/logo/links</header>
+   <main class="cont-main">Aqui as coisas acontecem e aparecem</main>
+   <section>aleatorio</section>
+   <footer>rodapé</footer>
+   </div>
+  )
+}
+
+export default App
